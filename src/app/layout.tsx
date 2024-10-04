79ef34bc7040fe46c4d86b0f065f024d2b9b845d
riyaz <riyaz3582@gmail.com>
2024-10-04 08:41:20 +0600

    fixed error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import Navbar2 from "@/_components/shared/Navbar/Navbar";
 import Footer from "@/_components/shared/Footer/Footer/Footer";
